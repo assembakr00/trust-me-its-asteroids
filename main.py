74c74dc0dc9bf4e2c76ee6122bdd636c834c45d1
Assem <assem.bakr@gmail.com>
2026-09-18 21:35:27 +0300

    Implement CircleShape and Player classes with movement and drawing functionality

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from logger import log_state
-
+import player
 
 def main():
     pygame.init()
@@ -12,6 +12,12 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+
+    player_obj = player.Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,7 +25,10 @@ def main():
                 return
 
         screen.fill("black")
+        player_obj.update(dt)
+        player_obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
